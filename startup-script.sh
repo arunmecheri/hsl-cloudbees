3accd5e7b46b1a728b2b8f8c0cd13419e6735b8a
arunmecheri <49418840+arunmecheri@users.noreply.github.com>
2019-04-09 09:31:51 -0700

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -6,8 +6,8 @@ sudo ufw allow 'Nginx Full'
 sudo ufw --force enable
 sudo systemctl reload nginx
 curl https://www.shiftleft.io/download/sl-latest-linux-x64.tar.gz > /tmp/sl.tar.gz && sudo tar -C /usr/local/bin -xzf /tmp/sl.tar.gz
-sl auth --org "fcf5e7a2-973f-4cff-b35b-62163909124d" --token "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NTk3Nzc3MDksImlzcyI6IlNoa$
-git clone https://github.com/alokshukla1978/HelloShiftLeft.git
+sl auth --org "0a4c8175-57fa-4b4c-91fb-b79b98d4cd3a" --token "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODU5NDYxODksImlzcyI6IlNoaWZ0TGVmdCIsIm9yZ0lEIjoiMGE0YzgxNzUtNTdmYS00YjRjLTkxZmItYjc5Yjk4ZDRjZDNhIiwic2NvcGVzIjpbImFwaTp2MiIsInVwbG9hZHM6d3JpdGUiLCJsb2c6d3JpdGUiLCJwaXBlbGluZXN0YXR1czpyZWFkIiwibG9nOndyaXRlIiwicG9saWNpZXM6cmVhZCJdfQ.TflrHbl4Ypdkj9fsNdy8YocDdFoGhfyWOSZiflYoSec1d2Rymwnx82fKUpU40UUzNcjxPU9113q3npZIYwZdxTJ3A_UXd136ohHBi8hC-ui9C9cGc5w7Cg-0GrWcjT1ixLRP5G1QL_GzU4ozDbbpiRwKeKhguNqGs24DhcIaWl8m1ZcsvloxCkUqFUYxxkb3YfFn7wpa7paRLOQC3ZV2fLzEVwDuoFCuiKrWNZ7GDBtgsR6vB_uP0KImIxqZJhRlYcvcumYvd5UEoZ4cHfwkQJjy35vIkhzEKMSBDiYaX8MHmt4wjbnhGIN8h94afr4tfAGpk-hDzqechw1f_LBAaQ"
+git clone https://github.com/arunmecheri/HelloShiftLeft.git
 cd HelloShiftLeft
 mvn clean package
 sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.old
